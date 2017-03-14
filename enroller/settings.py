a8f4cd6a0bf8dd9821a6a677639e0e0d1d6b792a
Griffin Lynch <Glynch1@cps.edu>
2017-03-14 13:39:12 -0500

    you know it

--- enroller/settings.py
+++ enroller/settings.py
@@ -127,8 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 import os.path
 
-STATIC_ROOT = 'static/'
-
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = ( os.path.join('static'), )
