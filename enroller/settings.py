a8cdd474282fb6f5a85aa8d6cfffe1cad52b74fe
Griffin Lynch <Glynch1@cps.edu>
2017-03-15 09:51:48 -0500

    one last time

--- enroller/settings.py
+++ enroller/settings.py
@@ -139,4 +139,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'wyregis@gmail.com'
-EMAIL_HOST_PASSWORD = 'sireedorsadswinger'
+EMAIL_HOST_PASSWORD = 'whitneyyoungisthebest'
