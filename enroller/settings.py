346d6e93d33f153c85b78fad8d8245090c9976e7
Griffin Lynch <Glynch1@cps.edu>
2017-03-22 12:36:29 -0500

    security?

--- enroller/settings.py
+++ enroller/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'enroller'
+    'enroller',
+    'djangosecure',
 ]
 
 SESSION_COOKIE_SECURE = True
@@ -51,8 +52,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.SecurityMiddleware',
 ]
 
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 ROOT_URLCONF = 'enroller.urls'
 
 TEMPLATES = [
